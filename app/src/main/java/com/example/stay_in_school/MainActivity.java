1852c086ef4552ed5ce833d56ee9c46e07602989
Anthony Justin Duong <anthony.duong@mail.utoronto.ca>
2020-05-20 13:42:48 -0400

    Added Bottom Navigation, using NavHost and Navigation Graph.

--- app/src/main/java/com/example/stay_in_school/MainActivity.java
+++ app/src/main/java/com/example/stay_in_school/MainActivity.java
@@ -2,29 +2,27 @@ package com.example.stay_in_school;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpBottomNav();
+    }
 
-        Button settingsButton = findViewById(R.id.settings_button);
-        Button thankButton = findViewById(R.id.thank_mr_goose);
-        Button editScheduleButton = findViewById(R.id.edit_schedule);
-
-        editScheduleButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent editScheduleActivity = new Intent(MainActivity.this, EditScheduleActivity.class);
-                startActivity(editScheduleActivity);
-            }
-        });
-
+    public void setUpBottomNav(){
+        bottomNavView = findViewById(R.id.bottom_nav);
+        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager()
+                .findFragmentById(R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavView,
+                navHostFragment.getNavController());
     }
 }
